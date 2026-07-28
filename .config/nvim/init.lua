1c133494887f1456c50fac13574afbe321149418
Micah Baker <micahb923@gmail.com>
2026-07-28 01:00:28 -0700

    Update .config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,9 @@
 vim.opt.syntax = "on"
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.colorcolumn = "80"
+vim.opt.wrap = false
+vim.opt.sidescroll = 1
+vim.opt.sidescrolloff = 8
 
 require("config.lazy")
